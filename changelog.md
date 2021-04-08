03b049aa8d245cf7dcd04b8eb8a3d6a488f8f49d
James MacDonald III <jamesdmacdonaldiii@gmail.com>
2021-04-07 22:41:23 -0400

    04/07/2021 22:38 Rebuilding get basic stock data script, in progress

--- changelog.md
+++ changelog.md
@@ -1,5 +1,8 @@
 # Changelog for in-progress branch
 
+## 04/07/2021 22:38
+Working on building the script to take all of the symbols in a list and fetch the data for those symbols. Attempting to make a slightly OOP approach, partly for practice, and partly to keep things neat and tidy. Unsure if my check_industry and check_sector methods work or not. Goal is to update the check_sector method to actually get it to assign a value to self.sector_id, then to finish building the loop and actually try to run it.
+
 ## 04/06/2021 07:48
 Returning to this project after a long break, which is largely because I had a lot of other learning to do before going much further with this one. I have now started the freeCodeCamp Tensorflow course, so I feel like I will actually be able to start making progress with this one soon. Changes to be implemented are listed below:
 
---
+++ stock.py
@@ -0,0 +1,76 @@
+# creating a stock class to make things more organized?
+
+import sqlite3
+import datetime
+import time
+
+class Stock:
+
+    def __init__(self, symbol, short_name=None, full_name=None, sector=None, industry=None, sector_id=None, industry_id=None):
+        self.symbol = symbol
+        # Everything else below here does not need to be defined upon creation
+        self.short_name = short_name
+        self.full_name = full_name
+        self.sector = sector
+        self.industry = industry
+        self.sector_id = sector_id
+        self.industry_id = industry_id
+
+    def check_industry(self, industry, conn):
+        '''This method takes the industry given as an input and checks to see if it exists in the database. If it does not already exist in the database, it adds it to the database.'''
+
+        industries = conn.execute('SELECT industry FROM industry')
+        industries = industries.fetchall()
+        
+        # loop thru all of the different industries in the industry table
+        # if the industry is found, we exit the while loop w/o adding it.
+        # if the industry is not found, we add it to the db
+        x = False
+        for i in industries:
+            if i == industry:
+                x = True
+                break
+        if x is False:
+            conn.execute(f'INSERT INTO industry (industry) VALUES ({industry})')
+            conn.commit()
+            industry_id = conn.execute(f'SELECT industry_id FROM industry WHERE industry = {industry}')
+            industry_id = industry_id.fetchall()
+            self.industry_id = industry_id[0]   # not quite sure if this is how this should work.
+
+    def check_sector(self, sector, conn):
+        '''This method takes the sector given as an input and checks to see if it exists in the database. If it does not already exist in the database, it adds it to the database.'''
+
+        sectors = conn.execute('SELECT sector FROM sector')
+        sectors = sectors.fetchall()
+        
+        # loop thru all of the different industries in the industry table
+        # if the industry is found, we exit the while loop w/o adding it.
+        # if the industry is not found, we add it to the db
+        x = False
+        for i in sectors:
+            if i == sectors:
+                x = True
+                break
+        if x is False:
+            conn.execute(f'INSERT INTO sector (sector) VALUES ({sector})')
+            conn.commit()
+
+class Price:
+
+    def __init__(self, stock, open_price, high_price, low_price, close_price, volume, price_datetime, dividends=None, stock_splits=None):
+        self.stock = stock  # this should be a stock object
+        self.open_price = open_price
+        self.high_price = high_price
+        self.low_price = low_price
+        self.close_price = close_price
+        self.volume = volume
+        self.price_datetime = price_datetime
+        # optional
+        self.dividends = dividends
+        self.stock_splits = stock_splits
+
+def get_current_datetime():
+    '''Determines the current Datetime in Year-Month-Day Hour:Min:Sec format and returns it as a string.'''
+    current_datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S"')
+    return current_datetime
+
